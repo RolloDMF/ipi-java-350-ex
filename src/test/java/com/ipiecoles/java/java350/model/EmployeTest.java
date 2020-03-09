e26f44fdb44f7ab68769ecdd56e27c21c829510a
Juriens Rodrigue <rodrigue.juriens@cgi.com>
2020-03-09 14:10:13 +0100

    exo 2

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -2,10 +2,10 @@ package com.ipiecoles.java.java350.model;
 
 import java.time.LocalDate;
 
-import com.ipiecoles.java.java350.model.Employe;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class EmployeTest {
 
@@ -33,5 +33,26 @@ public class EmployeTest {
 
       //Then
       Assertions.assertThat(nbAnnees).isEqualTo(0);
-}
+    }
+
+    @ParameterizedTest(name = "matricule {0} et performance {1} et dateEmbauche {2} et Temp parciel {3} donne une prime de : {4}")
+    @CsvSource({
+        "'M22222', 1, 5, 1.0, 2200.0",
+        "'T33333', 2, 10, 0.8, 2640.0"
+    })
+    // Matricule, performance, date d embauche, temps partiel, prime
+    public void testPrimeAnnuel(String matricule, Integer performance, Integer dateEmbauche, Double tempPartiel, Double prime) {
+        // Given
+        final Employe employe = new Employe();
+        employe.setDateEmbauche(LocalDate.now().minusYears(dateEmbauche));
+        employe.setMatricule(matricule);
+        employe.setPerformance(performance);
+        employe.setTempsPartiel(tempPartiel);
+
+        // When
+        final Double vraiPrime = employe.getPrimeAnnuelle();
+
+        //Then
+        Assertions.assertThat(vraiPrime).isEqualTo(prime);
+    }
 }
